6de6cdb183204f2c2246e93bfbf58397bf0fa879
guswns0508 <guswns0508@naver.com>
2023-03-29 21:37:05 +0900

    Create background.js

---
+++ js/background.js
@@ -0,0 +1,15 @@
+const images = [
+  "rabbit.jpg",
+  "mushroom.jpg",
+  "lake.jpg",
+  "sportscar.jpg",
+  "sea.jpg",
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
