5dd9026ba91d0cf77d306510b54a0b421e09b662
guswns0508 <guswns0508@naver.com>
2023-03-31 16:59:05 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,13 @@
-const clock = document.querySelector("h2.clock");
+const clock = document.querySelector("span.clock");
 
 function getClock() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}`;
+  clock.innerText = `${
+    date.getMonth() + 1
+  }월 ${date.getDate()}일 ${hours}:${minutes}`;
 }
 
 getClock();
